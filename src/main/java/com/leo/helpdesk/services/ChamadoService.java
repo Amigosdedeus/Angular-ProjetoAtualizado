9969326446832cd0c4d06abb53adfaa03042e02c
leonardo <leonardodanielbertilho@gmail.com>
2024-08-17 12:19:29 -0300

    findById para Chamado

---
+++ src/main/java/com/leo/helpdesk/services/ChamadoService.java
@@ -0,0 +1,26 @@
+package com.leo.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leo.helpdesk.domain.Chamado;
+import com.leo.helpdesk.repositories.ChamadoRepository;
+import com.leo.helpdesk.services.exceptions.ObjectnotFoundException;
+
+
+@Service
+public class ChamadoService {
+   
+    @Autowired
+    private ChamadoRepository repository;
+    
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID:" +id));
+    }
+ 
+
+	
+}
