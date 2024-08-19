d54cc062ea0956fd935184b4d8301f390d19ea8a
leonardo <leonardodanielbertilho@gmail.com>
2024-08-19 11:45:07 -0300

    create para Chamado

--- src/main/java/com/leo/helpdesk/domain/enums/Prioridade.java
+++ src/main/java/com/leo/helpdesk/domain/enums/Prioridade.java
@@ -1,35 +1,36 @@
 package com.leo.helpdesk.domain.enums;
 
 public enum Prioridade {
-    BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
 
-    private Integer codigo;
-    private String descricao;
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Prioridade(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
 
-    private Prioridade(Integer codigo, String descricao) {
-        this.codigo = codigo;
-        this.descricao = descricao;
-    }
+	public Integer getCodigo() {
+		return codigo;
+	}
 
-    public Integer getCodigo() {
-        return codigo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public static Perfil ToEnum(Integer cod) {
-        if (cod == null) {
-            return null;
-        }
-
-        for (Perfil x : Perfil.values()) {
-            if (cod.equals(x.getCodigo())) {
-                return x;
-            }
-        }
-
-        throw new IllegalArgumentException("Prioridade inválido!");
-    }
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Prioridade toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Prioridade x : Prioridade.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Prioridade inválida");
+	}
 }
--- src/main/java/com/leo/helpdesk/services/ChamadoService.java
+++ src/main/java/com/leo/helpdesk/services/ChamadoService.java
@@ -7,8 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leo.helpdesk.domain.Chamado;
+import com.leo.helpdesk.domain.Cliente;
+import com.leo.helpdesk.domain.Tecnico;
+import com.leo.helpdesk.domain.dtos.ChamadoDTO;
+import com.leo.helpdesk.domain.enums.Prioridade;
+import com.leo.helpdesk.domain.enums.Status;
 import com.leo.helpdesk.repositories.ChamadoRepository;
 import com.leo.helpdesk.services.exceptions.ObjectnotFoundException;
+
+import jakarta.validation.Valid;
 
 
 @Service
@@ -16,6 +23,10 @@ public class ChamadoService {
    
     @Autowired
     private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
     
     public Chamado findById(Integer id){
         Optional<Chamado> obj = repository.findById(id);
@@ -25,7 +36,29 @@ public class ChamadoService {
     public List<Chamado> findAll() {
       return repository.findAll();
     }
- 
+
+    public Chamado create(@Valid ChamadoDTO objDTO) {
+       return repository.save(newChamado(objDTO));
+    }
+    
+    private Chamado newChamado(ChamadoDTO obj){
+       Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+       Cliente cliente = clienteService.findById(obj.getCliente());
+
+       Chamado chamado = new Chamado();
+       if(obj.getId() != null ){
+          chamado.setId(obj.getId());
+       }
+
+       chamado.setTecnico(tecnico);
+       chamado.setCliente(cliente);
+       chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+       chamado.setStatus(Status.toEnum(obj.getStatus()));
+       chamado.setTitulo(obj.getTitulo());
+       chamado.setObservacoes(obj.getObservacoes());
+       return chamado;
+        
+    }
 
 	
 }
